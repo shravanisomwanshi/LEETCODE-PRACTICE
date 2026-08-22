ea9bc64eab107ac25f752c622d92373fb20d014e
Shravani Somwanshi <shravanisomwanshi88@gmail.com>
2026-08-22 20:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (82.30%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,17 +5,14 @@ class Solution(object):
         :type target: int
         :type rtype: List[int]
         """
-        # Gördüğümüz sayıları ve indekslerini tutmak için bir sözlük (Hash Map)
         seen = {}
         
         for index, num in enumerate(nums):
-            # Hedefe ulaşmak için ihtiyacımız olan diğer sayı
             complement = target - num
             
-            # Eğer bu tamamlayıcı sayı daha önce sözlüğe eklendiyse, çözümü bulduk!
+            
             if complement in seen:
                 return [seen[complement], index]
-            
-            # Eğer yoksa, mevcut sayıyı ve indeksini sözlüğe kaydet
+        
             seen[num] = index
-            #16.7.2026 yusuf arslantas
+            
